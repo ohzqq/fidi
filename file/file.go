fd7373091920b70d80a5cdadd044853be9c35693
ohzqq <iamchurkey@gmail.com>
2023-01-12 13:49:36 -0500

    file

--- file/file.go
+++ file/file.go
@@ -11,106 +11,171 @@ import (
 	"strings"
 )
 
-func FileExist(path string) bool {
-	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-		return false
-	}
-	return true
-}
-
 type File struct {
-	*FileName
-	Abs      string
-	Base     string
-	File     string
-	Mimetype string
+	*Name
+	Abs  string
+	Base string
+	Mime string
+	Stat os.FileInfo
+	data []byte
+	file *os.File
 }
 
-type FileName struct {
-	Path    string
-	Ext     string
-	Name    string
-	Padding string
-	data    []byte
-	file    *os.File
+type Name struct {
+	Dir       string
+	Extension string
+	Root      string
+	padding   string
+	pad       bool
+	prefix    string
+	suffix    string
+	num       int
+	min       int
+	max       int
 }
 
-func NewFile(n string) File {
+func New(n string) File {
 	abs, err := filepath.Abs(n)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f := File{
-		Base:     filepath.Base(abs),
-		Abs:      abs,
-		FileName: NewFileName(),
+	stat, err := os.Stat(abs)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	f.Ext = filepath.Ext(abs)
-	f.Mimetype = mime.TypeByExtension(f.Ext)
-	f.Name = strings.TrimSuffix(f.Base, f.Ext)
+	file := File{
+		Stat: stat,
+		Abs:  abs,
+		Base: filepath.Base(abs),
+		Name: &Name{},
+	}
 
-	f.Path, f.File = filepath.Split(abs)
+	if !file.Stat.IsDir() {
+		file.Extension = filepath.Ext(abs)
+		file.Root = strings.TrimSuffix(file.Base, file.Extension)
+		file.Mime = mime.TypeByExtension(file.Extension)
+		file.Dir = filepath.Dir(abs)
+	} else {
+		file.Root = file.Base
+		file.pad = true
+		file.Dir = abs
+	}
 
-	return f
+	return file
 }
 
-func NewFileName() *FileName {
-	name := &FileName{
-		Padding: "%03d",
+func NewName(n string) *Name {
+	name := &Name{
+		padding: "%03d",
+		Root:    n,
+		num:     1,
 	}
 	return name
 }
 
-func (f File) NewName() *FileName {
-	name := &FileName{
-		Name:    f.Name,
-		Path:    f.Path,
-		Padding: f.Padding,
-		//Ext:     f.Ext,
+func (n Name) Copy(names ...string) *Name {
+	name := &Name{
+		Root:    n.Root,
+		Dir:     n.Dir,
+		padding: n.padding,
+		pad:     n.pad,
+	}
+	if len(names) > 0 {
+		name.Root = names[0]
 	}
 	return name
 }
 
-func (f *FileName) WithExt(e string) *FileName {
-	f.Ext = e
-	return f
+func (n Name) Rename(root string) *Name {
+	name := n.Copy(root)
+	name.Extension = n.Extension
+	return name
 }
 
-func (f *FileName) Suffix(suf string) *FileName {
-	f.Name = f.Name + suf
-	return f
+func (n Name) Generate(bounds ...int) []*Name {
+	var min, max int
+	switch len(bounds) {
+	case 2:
+		max = bounds[1]
+		fallthrough
+	case 1:
+		min = bounds[0]
+	default:
+		min = 1
+		max = 100
+	}
+
+	var names []*Name
+	for i := min; i <= max; i++ {
+		n := n.Copy().Num(i).Ext(n.Extension)
+		names = append(names, n)
+	}
+
+	return names
 }
 
-func (f *FileName) Prefix(pre string) *FileName {
-	f.Name = pre + f.Name
-	return f
+func (n *Name) Ext(e string) *Name {
+	n.Extension = e
+	return n
 }
 
-func (f *FileName) Pad(i int) *FileName {
-	p := fmt.Sprintf(f.Padding, i)
-	return f.Suffix(p)
+func (n *Name) Suffix(suf string) *Name {
+	n.suffix = suf
+	return n
 }
 
-func (f FileName) Join() string {
-	return filepath.Join(f.Path, f.Name+f.Ext)
+func (n *Name) Prefix(pre string) *Name {
+	n.prefix = pre
+	return n
 }
 
-func (f FileName) Write(wr io.Writer) error {
-	_, err := wr.Write(f.data)
+func (n *Name) Pad(p string) *Name {
+	n.padding = p
+	n.pad = true
+	return n
+}
+
+func (n *Name) Num(i int) *Name {
+	n.pad = true
+	n.num = i
+	return n
+}
+
+func (n Name) Path() string {
+	name := fmt.Sprintf("%s%s%s", n.prefix, n.Root, n.suffix)
+
+	var padding string
+	if n.pad {
+		padding = fmt.Sprintf(n.padding, n.num)
+	}
+
+	name = fmt.Sprintf("%s%s%s", name, padding, n.Extension)
+
+	name = filepath.Join(n.Dir, name)
+
+	return name
+}
+
+func (n Name) String() string {
+	return n.Path()
+}
+
+func (i File) Write(wr io.Writer) error {
+	_, err := wr.Write(i.data)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (f FileName) Run() error {
-	if f.file != nil {
-		defer f.file.Close()
+func (i File) Run() error {
+	if i.file != nil {
+		defer i.file.Close()
 	}
 
-	err := f.Write(f.file)
+	err := i.Write(i.file)
 	if err != nil {
 		return err
 	}
@@ -118,20 +183,27 @@ func (f FileName) Run() error {
 	return nil
 }
 
-func (f *FileName) Tmp(data []byte) {
-	file, err := os.CreateTemp("", f.Name)
+func (i *File) Tmp(data []byte) {
+	file, err := os.CreateTemp("", i.Root)
 	if err != nil {
 		log.Fatal(err)
 	}
-	f.file = file
-	f.data = data
+	i.file = file
+	i.data = data
 }
 
-func (f *FileName) Save(data []byte) {
-	file, err := os.Create(f.Join())
+func (i *File) Save(data []byte) {
+	file, err := os.Create(i.String())
 	if err != nil {
 		log.Fatal(err)
 	}
-	f.file = file
-	f.data = data
+	i.file = file
+	i.data = data
+}
+
+func Exist(path string) bool {
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return true
 }
